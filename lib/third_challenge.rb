b6aecc8bf1b83ac3361c4feb42f93af17899609d
Ashley Bland <ashleyjbland@gmail.com>
2018-09-06 23:35:27 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,24 +8,15 @@ def third_challenge
         name: "Lady Montague",
         age: "54"
         },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
+        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {name: "Lord Capulet", age: "50"},
+        :matriarch => {name: "Lady Capulet", age: "51"},
+        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :heroine_friends => []
+     }
   }
-
 
 end
